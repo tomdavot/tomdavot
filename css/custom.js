4341cdeb96ff43784499ab1677ed4a74da2101c1
tomdavot <tomdavot.grange@gmail.com>
2023-06-06 18:52:37 +0200

    filters

--- css/custom.js
+++ css/custom.js
@@ -5,9 +5,21 @@ document.getElementById('keyword-menu').addEventListener('click', function(e) {
     // 	filterArticles(keyword);
     // 	setActiveItem(e.target);
     // }
+    
+    if(e.target && e.target.className === 'category-header'){
+
+	
+	var keyword = e.target.parentNode.parentNode.getAttribute('data-keyword');
+	filterArticles(keyword);
+
+	if(e.target.tagName === 'DIV') {
+	    setActiveItem(e.target.parentNode.parentNode);
+	}
+
+
+    }
 
     if (e.target && e.target.className === 'dropdownlink') {
-
 	var keyword = e.target.parentNode.getAttribute('data-keyword');
 	filterArticles(keyword);
 
@@ -17,8 +29,7 @@ document.getElementById('keyword-menu').addEventListener('click', function(e) {
 
 	
     }
-});   
-
+});
 
 
 // Fonction pour filtrer les articles en fonction des mots-clés
@@ -69,6 +80,7 @@ function setActiveItem(item) {
   
   for (var i = 0; i < items.length; i++) {
       items[i].classList.remove('active');
+      items[i].classList.add('inactive');
   }
 
     var submenu = document.getElementsByClassName('submenuItems');
@@ -76,8 +88,10 @@ function setActiveItem(item) {
     for(var i = 0; i < submenu.length; i++){
 	submenu[i].style.display = 'none';
     }
-  
+
+    item.classList.remove('inactive');
     item.classList.add('active');
+    
 
     
     for (var i = 0; i < item.childNodes.length; i++) {
@@ -102,4 +116,34 @@ function setActiveItem(item) {
 
 // Afficher tous les articles au chargement de la page
 filterArticles('all');
+
+
+document.getElementById('type-container').addEventListener('click', function(e) {
+    
+    
+    if(e.target && e.target.tagName === 'A' && e.target.parentNode.className === 'btn'){
+	filterByType(e.target.textContent);
+    }
+
+    
+
+    
+});
+
+
+function filterByType(type) {
+
+  var sec = document.getElementsByClassName('h-article');
+    for (var i = 0; i < sec.length; i++) {
+	sec[i].parentNode.style.display = 'block';
+    }
+    if(type != 'All')
+    for (var i = 0; i < sec.length; i++) {
+	if(type != sec[i].textContent){
+	    sec[i].parentNode.style.display = 'none';
+	} 
+
+    }
+
+}
 
